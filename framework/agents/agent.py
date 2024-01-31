580c8ca8044fa91b701ee9bdca2efc4b2849cec2
jonas-becker <snakeeye98@web.de>
2024-01-31 17:50:00 +0100

    use dbm as memory bucket instead of xml (more efficient)

--- framework/agents/agent.py
+++ framework/agents/agent.py
@@ -1,43 +1,63 @@
-import xml.etree.ElementTree as ET
 import glob, os
+from setup import *
+import dbm
+import json
+import fire
 
 class Agent():
     def __init__(self, id, llm_model, persona):
         self.id = id
         self.llm = llm_model
         self.persona = persona
-        self.memory_bucket= "/beegfs/wahle/github/MALLM/experiments/memory_bucket/{}.xml".format(self.id)
-        root = ET.Element("root")
-        tree = ET.ElementTree(root)
-        ET.SubElement(root, 'agent_id').text = str(self.id)
-        ET.SubElement(root, 'model').text = "LLM"
-        ET.SubElement(root, 'persona').text = self.persona
-        memory = ET.SubElement(root, 'memory')
-        tree.write(self.memory_bucket)
+        self.memory_bucket = memory_bucket_dir+"agent_{}".format(self.id)
+    
+    def __del__(self):
+        self.saveMemoryToJson()
 
     def createDraft():
         return None
 
-    def updateMemory(self, agent_id, turn, text):
-        tree = ET.parse(self.memory_bucket)
-        root = tree.getroot()
-        memory = root.find("memory")
-        
-        # Append the new text to the existing memory
-        text_element = ET.SubElement(memory, 'text')
-        text_element.set('agent_id', str(agent_id))
-        text_element.set('turn', str(turn))
-        text_element.text = text
-        
-        tree.write(self.memory_bucket)
+    def updateMemory(self, unique_id, turn, agent_id, agent_persona, text):
+        with dbm.open(self.memory_bucket, 'c') as db:
+            db[str(unique_id)] = f'{{"turn": {turn}, "agent_id": {agent_id}, "agent_persona": "{agent_persona}", "text": "{text}"}}'
+        self.saveMemoryToJson()
+    
+    def getMemory(self):
+        '''
+        Retrieves memory from the agents memory bucket as a dictionary
+        Returns: dict
+        '''
+        memory = {}
+        with dbm.open(self.memory_bucket, 'r') as db:
+            for key in db.keys():
+                memory[key.decode()] = db[key].decode()
+        return memory
+    
+    def saveMemoryToJson(self):
+        '''
+        Converts the memory bucket dbm data to json format
+        '''
+        try:
+            with open(self.memory_bucket+".json", "w") as f: 
+                json.dump(self.getMemory(), f)
+        except Exception as e:
+            print(f"Failed to save agent memory to {self.memory_bucket}: {e}")
 
-
-filelist = glob.glob(os.path.join("../../experiments/memory_bucket/", "*.xml"))
+filelist = glob.glob(os.path.join(memory_bucket_dir, "*.bak"))
+for f in filelist:
+    os.remove(f)
+filelist = glob.glob(os.path.join(memory_bucket_dir, "*.dat"))
+for f in filelist:
+    os.remove(f)
+filelist = glob.glob(os.path.join(memory_bucket_dir, "*.dir"))
+for f in filelist:
+    os.remove(f)
+filelist = glob.glob(os.path.join(memory_bucket_dir, "*.json"))
 for f in filelist:
     os.remove(f)
 
-agent = Agent(1, "Model Placeholder", "Health Minister")
-agent.updateMemory(1, 0, "I like tea.")
-agent.updateMemory(2, 1, "I like milk.")
-agent.updateMemory(1, 2, "Tea and milk is nice.")
-agent.updateMemory(2, 3, "R u bri'ish?")
+def main():
+    pass
+
+if __name__ == "__main__":
+    fire.Fire(main)
