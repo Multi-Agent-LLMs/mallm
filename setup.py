1270a3d6ca998f03d42757f6a802179342038042
lkaesberg <larskaesberg@yahoo.de>
2024-04-09 01:56:08 +0200

    fix: syntax only works with python3.12 and mallm needs to be installed as a package

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.12',
     install_requires=[
         "fire",
         "tqdm",
