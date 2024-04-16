4f8810277deb4b8bba4468026977964ddf2f5e51
lkaesberg <larskaesberg@yahoo.de>
2024-04-16 17:50:21 +0200

    feat: extract votes and definitions for acts

--- experiments/data/data_downloaders/EUCouncilVotes.py
+++ experiments/data/data_downloaders/EUCouncilVotes.py
@@ -1,5 +1,6 @@
 import gzip
 import json
+import os
 import shutil
 import urllib
 import uuid
@@ -7,6 +8,20 @@ import zipfile
 from rdflib import Graph
 
 from experiments.data.data_download import DatasetDownloader
+
+
+def process_ttl_gz(path: str):
+    print(f"Processing {path}")
+    if os.path.exists(path + ".json"):
+        file = open(path + ".json")
+        data = json.load(file)
+        return data
+    g = Graph()
+    g.parse(path + ".ttl")
+    g.serialize(path + ".json", format="json-ld")
+    f = open(path + ".json")
+    data = json.load(f)
+    return data
 
 
 class EUCouncilVotesDownloader(DatasetDownloader):
@@ -19,19 +34,44 @@ class EUCouncilVotesDownloader(DatasetDownloader):
         with gzip.open(self.dataset_path + "/VotingResults.ttl.gz", 'rb') as f_in:
             with open(self.dataset_path + "/VotingResults.ttl", 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
-        g = Graph()
-        g.parse(self.dataset_path + "/VotingResults.ttl")
-        v = g.serialize(self.dataset_path + "/VotingResults.json", format="json-ld")
-        f = open(self.dataset_path + "/VotingResults.json")
-        v = json.load(f)
-        print(len(v))
-        print(v[0])
-        v = [x for x in v if "http://www.w3.org/2004/02/skos/core#definition" in x]
-        print(len(v))
-        print(v[0]["@type"])
-        v = [x for x in v if "http://data.consilium.europa.eu/data/public_voting/rdf/schema/Act" in x["@type"]]
-        print(len(v))
+        # Needed for future additions
+        voting_results = process_ttl_gz(self.dataset_path + "/VotingResults")
+        voting_results_cube = process_ttl_gz(self.dataset_path + "/VotingResultsCube")
+        voting_results_new = process_ttl_gz(self.dataset_path + "/VotingResultsNew")
 
+        votes = []
+        definitions = [x for x in voting_results_cube if "http://www.w3.org/2004/02/skos/core#definition" in x]
+        for act in definitions:
+            act_id = act["@id"]
+            votes = [x for x in voting_results_cube if
+                     "http://data.consilium.europa.eu/data/public_voting/qb/dimensionproperty/act" in x and
+                     "@id" in x["http://data.consilium.europa.eu/data/public_voting/qb/dimensionproperty/act"][0] and
+                     x["http://data.consilium.europa.eu/data/public_voting/qb/dimensionproperty/act"][0][
+                         "@id"] == act_id]
+            if len(votes) == 0:
+                continue
+            votes_infavour = [vote for vote in votes if
+                              vote["http://data.consilium.europa.eu/data/public_voting/qb/measureproperty/vote"][0][
+                                  "@id"].split("/")[-1] == "votedinfavour"]
+            votes_against = [vote for vote in votes if
+                             vote["http://data.consilium.europa.eu/data/public_voting/qb/measureproperty/vote"][0][
+                                 "@id"].split("/")[-1] == "votedagainst"]
+            votes_abstained = [vote for vote in votes if
+                               vote["http://data.consilium.europa.eu/data/public_voting/qb/measureproperty/vote"][0][
+                                   "@id"].split("/")[-1] == "abstained"]
+
+            print(
+                f"Found {len(votes)} votes for {act_id} with {len(votes_infavour)} in favour, {len(votes_against)} against and {len(votes_abstained)} abstained ")
+            print(act["http://www.w3.org/2004/02/skos/core#definition"][0]["@value"])
+            print(f"{votes[0]["http://data.consilium.europa.eu/data/public_voting/qb/dimensionproperty/votingrule"][0]["@id"].split("/")[-1]}")
+
+        # print(len(v))
+        # print(v[0])
+        # v = [x for x in v if "http://www.w3.org/2004/02/skos/core#definition" in x]
+        # print(len(v))
+        # print(v[0]["@type"])
+        # v = [x for x in v if "http://data.consilium.europa.eu/data/public_voting/rdf/schema/Act" in x["@type"]]
+        # print(len(v))
 
     def __init__(self):
         super().__init__('eu_council_votes', hf_dataset=False)
