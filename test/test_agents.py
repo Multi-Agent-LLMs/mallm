e7823e03741ec0722d0dfc31448bb3fc8c7b2aea
Jonas Becker <snakeeye98@web.de>
2024-05-09 14:52:08 +0200

    python 3.12 and removed fixture

--- test/test_agents.py
+++ test/test_agents.py
@@ -3,13 +3,11 @@ from mallm.agents.panelist import *
 from mallm.agents.moderator import *
 
 
-@pytest.fixture()
 def test_agent_initialization():
     agent = Panelist()
     assert isinstance(agent, Panelist), "Panelist instance is not created properly."
 
 
-@pytest.fixture()
 def test_agent_initialization():
     agent = Moderator()
     assert isinstance(agent, Moderator), "Panelist instance is not created properly."
