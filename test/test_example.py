f8184b20e10438e33bd9e1325ad1c0642f1b87b5
Jonas Becker <snakeeye98@web.de>
2024-05-09 14:18:01 +0200

    Update test_example.py

--- test/test_example.py
+++ test/test_example.py
@@ -1,8 +1,15 @@
 import pytest
-from mallm.agents import Agent
+from mallm.agents.panelist import Panelist
+from mallm.agents.moderator import Moderator
 
 
 @pytest.fixture()
 def test_agent_initialization():
-    agent = Agent()
-    assert isinstance(agent, Agent), "Agent instance is not created properly."
+    agent = Panelist()
+    assert isinstance(agent, Panelist), "Panelist instance is not created properly."
+
+
+@pytest.fixture()
+def test_agent_initialization():
+    agent = Moderator()
+    assert isinstance(agent, Moderator), "Panelist instance is not created properly."
