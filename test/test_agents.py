a20cc636104344ae26639d5c54523d0d4fc9a643
Jonas Becker <snakeeye98@web.de>
2024-05-09 14:59:43 +0200

    +1

--- test/test_agents.py
+++ test/test_agents.py
@@ -4,10 +4,10 @@ from mallm.agents.moderator import *
 
 
 def test_agent_initialization():
-    agent = Panelist(None, None, None, None, None, None)
+    agent = Panelist(None, None, None, None, None)
     assert isinstance(agent, Panelist), "Panelist instance is not created properly."
 
 
 def test_agent_initialization():
-    agent = Moderator(None, None, None, None, None, None)
+    agent = Moderator(None, None, None, None, None)
     assert isinstance(agent, Moderator), "Panelist instance is not created properly."
