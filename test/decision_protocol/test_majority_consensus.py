c861589e84892475387d59bf82927bb66378c616
lkaesberg <larskaesberg@yahoo.de>
2024-05-17 00:33:16 +0200

    fix: tests for new protocol structure

--- test/decision_protocol/test_majority_consensus.py
+++ test/decision_protocol/test_majority_consensus.py
@@ -9,52 +9,30 @@ panelists = [Panelist(None, None, coordinator, f"Person{i}", "") for i in range(
 
 def test_unanimous_decision_in_first_five_turns():
     mc = MajorityConsensus(panelists[:3])
-    agreements = [
-        Agreement(agreement=True, agent_id="", persona="", response="")
-        for _ in range(3)
-    ]
-    decision, is_consensus = mc.make_decision(agreements, 4)
+    agreements = [Agreement(agreement=True, agent_id="", persona="", response="") for _ in range(3)]
+    decision, is_consensus = mc.make_decision(agreements, 4, "", "")
     assert is_consensus
 
 
 def test_no_unanimous_decision_in_first_five_turns():
     mc = MajorityConsensus(panelists[:3])
-    agreements = [
-        Agreement(agreement=True, agent_id="", persona="", response="")
-        for _ in range(2)
-    ]
+    agreements = [Agreement(agreement=True, agent_id="", persona="", response="") for _ in range(2)]
     agreements.append(Agreement(agreement=False, agent_id="", persona="", response=""))
-    decision, is_consensus = mc.make_decision(agreements, 4)
+    decision, is_consensus = mc.make_decision(agreements, 4, "", "")
     assert not is_consensus
 
 
 def test_majority_decision_after_five_turns():
     mc = MajorityConsensus(panelists)
-    agreements = [
-        Agreement(agreement=True, agent_id="", persona="", response="")
-        for _ in range(3)
-    ]
-    agreements.extend(
-        [
-            Agreement(agreement=False, agent_id="", persona="", response="")
-            for _ in range(2)
-        ]
-    )
-    decision, is_consensus = mc.make_decision(agreements, 6)
+    agreements = [Agreement(agreement=True, agent_id="", persona="", response="") for _ in range(3)]
+    agreements.extend([Agreement(agreement=False, agent_id="", persona="", response="") for _ in range(2)])
+    decision, is_consensus = mc.make_decision(agreements, 6, "", "")
     assert is_consensus
 
 
 def test_no_majority_decision_after_five_turns():
     mc = MajorityConsensus(panelists)
-    agreements = [
-        Agreement(agreement=True, agent_id="", persona="", response="")
-        for _ in range(2)
-    ]
-    agreements.extend(
-        [
-            Agreement(agreement=False, agent_id="", persona="", response="")
-            for _ in range(3)
-        ]
-    )
-    decision, is_consensus = mc.make_decision(agreements, 6)
+    agreements = [Agreement(agreement=True, agent_id="", persona="", response="") for _ in range(2)]
+    agreements.extend([Agreement(agreement=False, agent_id="", persona="", response="") for _ in range(3)])
+    decision, is_consensus = mc.make_decision(agreements, 6, "", "")
     assert not is_consensus
