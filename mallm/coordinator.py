573460666c357988475dbf1dc69ad4f1df5b7e6d
Lars Benedikt Kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-05-16 16:53:52 +0200

    fix: removed star imports

--- mallm/coordinator.py
+++ mallm/coordinator.py
@@ -1,10 +1,16 @@
+import dbm
+import json
+import logging
+import os
 import time
+import uuid
 from datetime import timedelta
 
+import fire
 import transformers
 
-from mallm.agents.moderator import *
-from mallm.agents.panelist import *
+from mallm.agents.moderator import Moderator
+from mallm.agents.panelist import Panelist
 from mallm.decision_making.DecisionProtocol import DecisionProtocol
 from mallm.decision_making.MajorityConsensus import MajorityConsensus
 from mallm.discourse_policy.DiscourceDebate import DiscourseDebate
@@ -13,6 +19,7 @@ from mallm.discourse_policy.DiscourceRelay import DiscourseRelay
 from mallm.discourse_policy.DiscourceReport import DiscourseReport
 from mallm.discourse_policy.DiscoursePolicy import DiscoursePolicy
 from mallm.models.personas.PersonaGenerator import PersonaGenerator
+from mallm.prompts.coordinator_prompts import generate_chat_prompt_extract_result
 
 transformers.logging.set_verbosity_error()
 os.environ["PL_TORCH_DISTRIBUTED_BACKEND"] = "gloo"
