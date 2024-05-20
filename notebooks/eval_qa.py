44ef30465998ba7227165eb4c4d371f2d4d0d10e
ItsNiklas <nikbau2000@gmail.com>
2024-05-20 16:58:30 +0200

    fix: better qa eval
    ruff gitignore

--- notebooks/eval_qa.py
+++ notebooks/eval_qa.py
@@ -22,26 +22,36 @@ with open(out_file, "r") as f:
     data = json.load(f)
 
 total_score = 0
+total_questions = 0
 
 for question in data:
     answer = question["answer"]
 
+    if answer is None:
+        print("\nNo answer found in question")
+        continue
+
     reference = question["references"][0]
 
     # print("Question:", question["input"])
-    print("\nAnswer:", answer)
-    print("Reference:", reference)
+    print('\nAnswer: "', answer, '"', sep="")
 
     match = re.search(ANSWER_PATTERN_MULTICHOICE, answer)
     # Backup
     if not match:
         match = re.search(r"(?i)([A-D])(\W|$)", answer)
 
+    if not match:
+        print("No match found in answer.")
+        continue
+
     print("Extracted answer:", match.group(1) if match else None)
     extracted_answer = match.group(1) if match else None
+    print("Reference:", reference)
     score = 1 if extracted_answer == reference else 0
     total_score += score
+    total_questions += 1
 
 print("\n\nTotal score:", total_score)
-print("Total questions:", len(data))
-print("Accuracy:", total_score / len(data))
+print("Total questions:", total_questions)
+print("Accuracy:", total_score / total_questions)
