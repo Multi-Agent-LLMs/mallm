cb4448b89daa210c007260fb50122e7c868ee8a4
Jonas Becker <56976889+jonas-becker@users.noreply.github.com>
2024-06-12 13:46:10 +0200

    fix num_agents not being applied to persona generator

--- mallm/coordinator.py
+++ mallm/coordinator.py
@@ -115,7 +115,9 @@ class Coordinator:
 
         personas = PERSONA_GENERATORS[self.agent_generator](
             llm=self.llm
-        ).generate_personas(f"{task_instruction} {input_str}", num_agents)
+        ).generate_personas(
+            task_description=f"{task_instruction} {input_str}", num_agents=num_agents
+        )
 
         if use_moderator:
             self.moderator = Moderator(self.llm, self.client, self)
