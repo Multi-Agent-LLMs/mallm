2c032d9e842975df98df33fe650d9144b265c15f
lkaesberg <larskaesberg@yahoo.de>
2024-06-13 20:39:34 +0200

    fix: return type supress

--- mallm/utils/utils.py
+++ mallm/utils/utils.py
@@ -1,10 +1,11 @@
 import os
 import sys
 from contextlib import contextmanager
+from typing import Generator
 
 
 @contextmanager
-def suppress_stdout() -> None:
+def suppress_stdout() -> Generator:
     with open(os.devnull, "w") as devnull:
         old_stdout = sys.stdout
         sys.stdout = devnull
