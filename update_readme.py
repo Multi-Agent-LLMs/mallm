5485bd54f3fbf22cdb2f8f50cc7da7a47b80f2e2
Jonas Becker <56976889+jonas-becker@users.noreply.github.com>
2024-06-18 17:59:31 +0200

    add workflow py script

---
+++ update_readme.py
@@ -0,0 +1,29 @@
+from mallm.utils.config import Config
+import re
+
+config = Config(data=None, out=None, instruction=None)
+attributes = {
+    attr: getattr(config, attr)
+    for attr in dir(config)
+    if not callable(getattr(config, attr)) and not attr.startswith("__")
+}
+
+with open("README.md", "r+") as readme_file:
+    content = readme_file.read()
+
+    attributes_content = ""
+    for attr, value in attributes.items():
+        attributes_content += f"{attr}: {value}\n"
+
+    replacement_str = "### Config Arguments:\n```py\n" + attributes_content + "```"
+    print(replacement_str)
+
+    updated_lines = re.sub(
+        r"### Config Arguments:\n```py\n(.*?)```",
+        replacement_str,
+        content,
+        flags=re.DOTALL,
+    )
+    readme_file.seek(0)
+    readme_file.writelines(updated_lines)
+    readme_file.truncate()
