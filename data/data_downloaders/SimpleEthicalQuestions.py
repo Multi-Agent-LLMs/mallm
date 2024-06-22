57494771090b3ce0245ccb32f5334408d304c025
Jonas Becker <56976889+jonas-becker@users.noreply.github.com>
2024-06-22 12:56:19 +0200

    fix ethical questions multiple choice context

--- data/data_downloaders/SimpleEthicalQuestions.py
+++ data/data_downloaders/SimpleEthicalQuestions.py
@@ -33,15 +33,16 @@ class SimpleEthicalQuestionsDownloader(DatasetDownloader):
         input_examples = []
         for s in self.dataset[: self.sample_size]:
             ref = [k for k, v in s["target_scores"].items() if v == 1]
-            multiple_choice_str = " Answer Choices:"
+            multiple_choices = []
             for i, (k, v) in enumerate(s["target_scores"].items()):
-                multiple_choice_str += " " + f"{chr(ord('A') + i)}) " + k
+                multiple_choices.append(f"{chr(ord('A') + i)}) " + k)
+            print(multiple_choices)
             input_examples.append(
                 InputExample(
                     example_id=str(uuid.uuid4()),
                     dataset_id=None,
                     inputs=[s["input"]],
-                    context=None,
+                    context=multiple_choices,
                     references=[ref[0]],
                     personas=None,
                 )
