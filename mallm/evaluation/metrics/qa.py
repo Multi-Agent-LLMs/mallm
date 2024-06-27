42a5e9ac20ea74b652c046b93895d7a3b20152e7
Jonas Becker <56976889+jonas-becker@users.noreply.github.com>
2024-06-27 18:30:40 +0200

    QA: also match against freetext, StrategyQA: cleaner reference text

--- mallm/evaluation/metrics/qa.py
+++ mallm/evaluation/metrics/qa.py
@@ -21,6 +21,7 @@ class MultiChoiceBoolean(Metric):
 
         reference = reference_texts[0][0]  # first character should always be the label
         match = re.search(MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE, generated_text)
+        extracted_answer = None
 
         if not match:
             match = re.search(
@@ -28,15 +29,25 @@ class MultiChoiceBoolean(Metric):
             )
 
         if not match:
-            logger.warning(f"No pattern match found in answer: {generated_text}")
+            reference = re.sub(
+                MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE, "", reference
+            ).strip()
+            reference = re.sub(
+                MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE_BACKUP, "", reference
+            ).strip()
+            match = re.search(reference, generated_text)
+            logger.debug(
+                f"No answer pattern detected. Trying to match against freetext reference: {generated_text}"
+            )
+        else:
+            extracted_answer = match.group(1)
+            logger.debug(f"Extracted answer: {match.group(1)} from {generated_text}")
+
+        if not match:
+            logger.warning(f"No match found in answer: {generated_text}")
             return {"correct": False}
 
-        logger.debug(f"Extracted answer: {match.group(1)} from {generated_text}")
-        logger.debug(f"Comparing against reference: {reference}")
-
-        extracted_answer = match.group(1)
-
-        score = extracted_answer == reference
+        score = extracted_answer == reference or reference in generated_text
 
         return {"correct": score}
 
