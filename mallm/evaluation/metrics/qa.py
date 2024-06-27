23dd9cc87e5feaaa9a40cd3b6d9ccdcf1d13615a
Jonas Becker <56976889+jonas-becker@users.noreply.github.com>
2024-06-27 18:30:40 +0200

    QA: also match against freetext, StrategyQA: cleaner reference text

--- mallm/evaluation/metrics/qa.py
+++ mallm/evaluation/metrics/qa.py
@@ -21,6 +21,7 @@ class MultiChoiceBoolean(Metric):
 
         reference = reference_texts[0]
         match = re.search(MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE, generated_text)
+        extracted_answer = None
 
         if not match:
             match = re.search(
@@ -28,14 +29,24 @@ class MultiChoiceBoolean(Metric):
             )
 
         if not match:
+            reference = re.sub(
+                MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE, "", reference
+            ).strip()
+            reference = re.sub(
+                MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE_BACKUP, "", reference
+            ).strip()
+            match = re.search(reference, generated_text)
+            logger.debug(
+                f"No answer pattern detected. Trying to match against freetext reference: {generated_text}"
+            )
+        else:
+            extracted_answer = match.group(1)
+            logger.debug(f"Extracted answer: {match.group(1)} from {generated_text}")
+
+        if not match:
             logger.warning(f"No match found in answer: {generated_text}")
-            return {"score": None}
+            return {"correct": False}
 
-        logger.debug(f"Extracted answer: {match.group(1)} from {generated_text}")
-
-        extracted_answer = match.group(1)
-
-        assert len(reference) == 1, "Reference should be a single character."
-        score = extracted_answer == reference
+        score = extracted_answer == reference or reference in generated_text
 
         return {"correct": score}
