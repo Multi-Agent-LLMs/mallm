f6d4584333d70d7b7fe8a17d2469b04e4b82e27a
Jonas Becker <snakeeye98@web.de>
2024-07-02 20:24:29 +0200

    revert freetext matching pattern matching can detect patterns in freetext still

--- mallm/evaluation/metrics/qa.py
+++ mallm/evaluation/metrics/qa.py
@@ -19,9 +19,8 @@ class MultiChoiceBoolean(Metric):
     @staticmethod
     def evaluate(generated_text: str, reference_texts: list[str]) -> dict[str, Any]:
 
-        reference = reference_texts[0]
+        reference = reference_texts[0][0]  # first character should always be the label
         match = re.search(MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE, generated_text)
-        extracted_answer = None
 
         if not match:
             match = re.search(
@@ -29,32 +28,17 @@ class MultiChoiceBoolean(Metric):
             )
 
         if not match:
-            reference = re.sub(
-                MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE, "", reference
-            ).strip()
-            reference = re.sub(
-                MultiChoiceBoolean.ANSWER_PATTERN_MULTICHOICE_BACKUP, "", reference
-            ).strip()
-            match = re.search(
-                reference.lower(), generated_text.lower(), flags=re.IGNORECASE
-            )
-        else:
-            extracted_answer = match.group(1)
-
-        if not match:
-            logger.debug(
-                f"No pattern match or text identity found in answer: {generated_text}, reference: {reference}"
-            )
+            logger.warning(f"No pattern match found in answer: {generated_text}")
             return {"correct": False}
-        if extracted_answer:
-            logger.debug(
-                f"Pattern match tested in answer: {generated_text}, reference: {reference}"
-            )
-            return {"correct": extracted_answer == reference}
-        logger.debug(
-            f"Freetext identity tested in answer: {generated_text}, reference: {reference}"
-        )
-        return {"correct": reference.lower() in generated_text.lower()}
+
+        logger.debug(f"Extracted answer: {match.group(1)} from {generated_text}")
+        logger.debug(f"Comparing against reference: {reference}")
+
+        extracted_answer = match.group(1)
+
+        score = extracted_answer == reference
+
+        return {"correct": score}
 
 
 class AnswerabilityBoolean(Metric):
