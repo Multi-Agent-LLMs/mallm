f8664b423f58cfe1153df3918efd3d03202e3dcb
Jonas Becker <snakeeye98@web.de>
2024-07-04 14:35:32 +0200

    prompt to also extract the multichoice letter

--- mallm/models/discussion/FreeTextResponseGenerator.py
+++ mallm/models/discussion/FreeTextResponseGenerator.py
@@ -148,7 +148,7 @@ Input: {input_str}
         current_prompt = [
             {
                 "role": "system",
-                "content": "Extract the final solution to the task from the provided text. Remove statements of agreement, disagreement, and explanations. Do not modify the text. Do not output any text besides the solution. If there is no solution provided, just copy the text.",
+                "content": "Extract the final solution to the task from the provided text. Remove statements of agreement, disagreement, and explanations. Do not modify the text. Do not output any text besides the solution. Include the letter (A, B, C, D) in the solution if it exists. If there is no solution provided, just copy the text.",
             },
             {
                 "role": "user",
