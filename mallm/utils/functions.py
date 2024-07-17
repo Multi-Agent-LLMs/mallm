e572c652bd5ba9514a9c7f6e5935816466716d73
Lars Benedikt Kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-07-17 14:05:43 +0200

    fix: use Path to read file

--- mallm/utils/functions.py
+++ mallm/utils/functions.py
@@ -23,10 +23,8 @@ def sort_output_file(input_file: str, output_file: str) -> None:
     Sorts the output file to match the input file.
     """
     print(f"Sorting output file {output_file} to match the input file {input_file}...")
-    with open(output_file) as file:
-        data_out = json.loads(file.readline())
-    with open(input_file) as file:
-        data_in = json.loads(file.readline())
+    data_out = json.loads(Path(output_file).read_text())
+    data_in = json.loads(Path(input_file).read_text())
 
     # Create a dictionary to map example_ids to their corresponding data_out entries
     data_out_dict = {entry["exampleId"]: entry for entry in data_out}
