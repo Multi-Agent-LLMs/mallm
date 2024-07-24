6f1a7ebc6a46bf631c12520b8ea2436f102548b2
Jonas Becker <snakeeye98@web.de>
2024-07-24 14:31:27 +0200

    also create memory buckets for agent

--- mallm/coordinator.py
+++ mallm/coordinator.py
@@ -48,8 +48,8 @@ class Coordinator:
         self.use_moderator = use_moderator
         self.moderator: Optional[Moderator] = None
         self.memory_bucket_dir = memory_bucket_dir
-        dbm.open(self.memory_bucket, "c")
         self.memory_bucket = os.path.join(self.memory_bucket_dir, "global_" + self.id)
+        dbm.open(self.memory_bucket, "c")
         self.decision_protocol: Optional[DecisionProtocol] = None
         self.llm = model
         self.response_generator: ResponseGenerator = SimpleResponseGenerator(self.llm)
